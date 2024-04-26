db2e8602450403a9e283117c78c1e42a26a61111
David De Los Santos <ddelossantos@neural.com.do>
2024-04-25 23:44:12 -0400

    Creating domain layer of the app Adding Book, BookInventory and Category classes

---
+++ Domain/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public DateTime PublishedDate { get; set; } = DateTime.UtcNow;
+        public required string ISBN { get; set; }
+        public string? Description { get; set; }
+        
+        public required Category Category { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
---
+++ Domain/BookInventory.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class BookInventory
+    {
+        public int Id { get; set; }
+        public required int BookId { get; set; }
+        public required Book Book { get; set; }
+        public int AvailableCopies { get; set; } = 0;
+    }
+}
