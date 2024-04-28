06840155abc959a588a67dcac04c50614a7f76c7
David De Los Santos <ddelossantos@neural.com.do>
2024-04-28 02:50:21 -0400

    Adding reference to api project
    Also, adding context

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseInMemoryDatabase("LibraryDb");
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
